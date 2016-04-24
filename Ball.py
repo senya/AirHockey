7f16455dfa027d66b46a607c00f8c7eff87fe724
addward <adddward@gmail.com>
2016-04-24 18:48:08 +0300

    Game field changes
    -Fence and 2 gates are added
    -Resolution is changed to 450x600
    -fps is increased to 70
    -Score is added
    -Ball.check_hit is now running in Main.move method not in Ball.update
    -New font is added to main directory

--- Ball.py
+++ Ball.py
@@ -3,57 +3,45 @@ import Player
 import math
 
 class Ball:
-    def __init__(self, player1, player2, Vx = 10, Vy = 10, x = 300, y = 300, a = 5, r = 12, color = (225,225,225)):
+    def __init__(self, Vx = 10, Vy = 10, x = 300, y = 300, a = 5, r = 12, color = (225,225,225)):
         self.Vx, self.Vy, self.x, self.y, self.a, self.r, self.color = Vx, Vy, x, y , a, r, color
-        self.player1, self.player2 = player1, player2
 
     def render(self,game):
         pygame.draw.circle(game.screen,self.color,(int(self.x),int(self.y)),self.r)
 
     def update(self,game):
-        self.check_hit(self.player1, game)
-        self.check_hit(self.player2, game)
         self.x += self.Vx * game.delta
         self.y += self.Vy * game.delta
 
     def check_hit(self, player, game):
         color = [(230,51,51),(54,116,225)]
-        if self.x+self.r>game.width:
-            self.x = game.width - self.r
+        """Удар с правой гранью экрана"""
+        if self.x + self.r + game.ethik>game.width:
+            self.x = game.width - game.ethik - self.r
             self.Vx =-self.Vx
-        if self.x-self.r<0:
-            self.x = self.r
+
+        """Удар с левой гранью экрана"""
+        if self.x - self.r - game.ethik<0:
+            self.x = self.r + game.ethik
             self.Vx = -self.Vx
-        if self.y+self.r>game.height:
-            self.y = game.height - self.r
+
+        """Удар с нижней гранью экрана"""
+        if self.y + self.r + game.ethik>game.height:
+            self.y = game.height - self.r - game.ethik
             self.Vy = -self.Vy
-        if self.y-self.r<0:
-            self.y = self.r
+
+        """Удар с верхней гранью экрана"""
+        if self.y-self.r - game.ethik<0:
+            self.y = self.r + game.ethik
             self.Vy = -self.Vy
-        if (self.x<=player.x+player.r) and (self.x>=player.x):
-            range = player.y-self.y # расстояние он верхней левой грани платформы до центра шарика
-            if (range<0):
-                #шарик снизу платвормы
-                if abs(range+player.r) <= self.r:
-                    if (abs(player.vy)>0):
-                        self.Vy = player.vy + abs(self.Vy)*abs(player.vy)/player.vy    #Считаем, что после удара скорость шарика = скорости платформы
-                    else:
-                        self.Vy = -self.Vy
-                    if (abs(player.vx)>0):
-                        self.Vx = player.vx/2 + self.Vx
-                    else:
-                        self.Vx = self.Vx
-                    self.y = player.y + player.r + self.r
-                    self.color = color[player._team]
-            else:
-                if (abs(range) <= self.r):
-                    if (abs(player.vy)>0):
-                        self.Vy = player.vy + abs(self.Vy)*abs(player.vy)/player.vy
-                    else:
-                        self.Vy = -self.Vy
-                    if (abs(player.vx)>0):
-                        self.Vx = player.vx/2  + self.Vx
-                    else:
-                        self.Vx = self.Vx
-                    self.y = player.y - self.r
-                    self.color = color[player._team]
+
+        """Удар с шаром игрока"""
+        if ((self.x - player.x)**2+(self.y - player.y)**2<(self.r+player.r)**2):
+            mod = math.sqrt((self.x - player.x)**2+(self.y - player.y)**2)       #Содуль вектора, соединящего центры шаров
+            cosa = (self.x-player.x)/mod                                         #Косинус угла наклона вектора, соединяющего центры шаров
+            sina = (self.y-player.y)/mod
+            self.x = player.x + (self.r+player.r)*(self.x-player.x)/mod
+            self.y = player.y + (self.r+player.r)*(self.y-player.y)/mod
+            self.color = color[player._team]
+            self.Vx = player.vx + self.Vx
+            self.Vy = player.vy + self.Vy
--- Main.py
+++ Main.py
@@ -7,25 +7,27 @@ class Game:
     def tick(self):
         """Return time in seconds since previous call
         and limit speed of the game to 50 fps"""
-        self.delta = self.clock.tick(50) / 1000.0
+        self.delta = self.clock.tick(70) / 1000.0
 
     def __init__(self):
         """Constructor of the Game"""
         self._running = True
-        self.size = self.width, self.height = 640, 600
+        pygame.init()
+        self.size = self.width, self.height = 450, 600
         # create main display - 640x400 window
         # try to use hardware acceleration
         self.screen = pygame.display.set_mode(self.size)#, pygame.HWSURFACE
-        #self.image = pygame.Surface((640,400))
-        # set window caption
         pygame.display.set_caption('Game')
-        # get object to help track time
         self.clock = pygame.time.Clock()
         # set default tool
         self.tool = 'run'
-        self.player  = Player.Player(1, x=220, y=370,r = 30 )
-        self.player2 = Player.Player(0, x=220, y=10,r = 30 )
-        self.ball    = Ball.Ball(player1=self.player, player2=self.player2)
+        self.player  = Player.Player(1, x=self.width/2, y=370,r = 30 )
+        self.player2 = Player.Player(0, x=self.width/2, y=50,r = 30 )
+        self.ball    = Ball.Ball()
+        self.ethik   = 10                                                  # Толщина отступов
+        self.ecolor = (255,179,0)
+        self.font = pygame.font.Font('materials/9013.ttf', 100)
+        self.gate = 40                                                     # Полудлина ворот
 
     def event_handler(self, event):
         """Handling one pygame event"""
@@ -41,17 +43,21 @@ class Game:
         """Here game objects update their positions"""
         self.tick()
         self.pressed = pygame.key.get_pressed()
-
+        self.ball.check_hit(self.player,self)
+        self.ball.check_hit(self.player2,self)
         self.ball.update(self)
         self.player.update(self)
         self.player2.update(self)
 
-
     def render(self):
         """Render the scene"""
         self.screen.fill((0, 0, 0))
-        #self.screen = pygame.image.load("images/back.png")
-        #self.image = pygame.image.load("images/back.png")
+        pygame.draw.rect(game.screen,self.ecolor,(0,0,self.width/2-self.gate,self.ethik))                                #Край верхней грани
+        pygame.draw.rect(game.screen,self.ecolor,(self.width/2+self.gate,0,self.width,self.ethik))                       ###################
+        pygame.draw.rect(game.screen,self.ecolor,(0,0,self.ethik,self.height))                                           # Край левой грани
+        pygame.draw.rect(game.screen,self.ecolor,(self.width-self.ethik,0,self.ethik,self.height))                       # Край правой грани
+        pygame.draw.rect(game.screen,self.ecolor,(0,self.height-self.ethik,self.width/2-self.gate,self.height))          # Край нижней грани
+        pygame.draw.rect(game.screen,self.ecolor,(self.width/2+self.gate,self.height-self.ethik,self.width,self.height)) ###################
         self.player.render(self)
         self.player2.render(self)
         self.ball.render(self)
